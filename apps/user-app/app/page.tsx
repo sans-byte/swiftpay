48f2b4fc709831a4b26be203b28a397107993474
sanskar jain <sanskarj15@gmail.com>
2024-08-24 12:45:43 +0530

    basic prisma configs

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,10 +1,5 @@
 import { PrismaClient } from "@repo/db/client";
-
 export default async function Home() {
   const client = new PrismaClient();
-
-  const res = await client.user.findMany();
-
-  console.log(res);
   return <div className="text-2xl">Hi there</div>;
 }
