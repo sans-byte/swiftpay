c9eff1f008dbab607c8201e9669ee710d11711cf
sanskar jain <sanskarj15@gmail.com>
2024-08-24 12:44:37 +0530

    prisma configuration

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/client";
 
-export default function Home() {
+export default async function Home() {
+  const client = new PrismaClient();
+
+  const res = await client.user.findMany();
+
+  console.log(res);
   return <div className="text-2xl">Hi there</div>;
 }
