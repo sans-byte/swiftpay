127bdae5796d4dfd1179b2159abbd62bf11b741e
sanskar jain <sanskarj15@gmail.com>
2024-08-24 22:27:57 +0530

    added and refacroted pages

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import { AppbarClient } from "./AppbarClient";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <AppbarClient />
+          {children}
+        </body>
       </Providers>
     </html>
   );
